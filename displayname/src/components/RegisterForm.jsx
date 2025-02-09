f3bc825d074a6ae8e9acf87e8c86cba1f9e3b651
Avinash Thakur <avinash.thakur2209@gmail.com>
2025-02-09 13:35:02 +0530

    Added Form Register component

---
+++ displayname/src/components/RegisterForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function RegisterForm(){
+    const [data, setData] = useState({
+        fname: "",
+        lname: ""
+    })
+    const [showName, setShowName] = useState(false);
+
+
+    function submitHandler(e){
+        e.preventDefault()
+        setShowName(true);
+    }
+
+    return (
+        <>
+        <h1>Full Name Display</h1>
+        <form onSubmit={submitHandler}>
+        <label htmlFor="fname">First Name:</label>
+        <input type="text" name="fname" id="fname" required onChange={(e) => setData({...data, fname:(e.target.value)})}/><br />
+        <label htmlFor="lname">Last Name:</label>
+        <input type="text" name="lname" id="lname" required onChange={(e) => setData({...data, lname:(e.target.value)})}/><br />
+        <button type="submit">Submit</button>
+        </form>
+        {
+            (showName) ? <span>Full Name : {data.fname} {data.lname}</span> : null
+        }
+        </>
+    )
+}
+
+export default RegisterForm;
